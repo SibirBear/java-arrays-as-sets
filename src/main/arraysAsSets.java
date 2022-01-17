d7d2849585bebc0f9f60a5159b512fab6416fa3c
SibirBear <c.igor@list.ru>
2022-01-18 00:22:04 +0700

    create java-arrays-as-sets

---
+++ src/main/arraysAsSets.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class arraysAsSets {
+
+    private final Object[] array;
+
+    public arraysAsSets(final Object[] array) {
+        this.array = array;
+    }
+
+    public static <T> T[] intersection(final T[]... arrays) {
+        Set<T> set = new HashSet<>(Arrays.asList(arrays[0]));
+        for (int i = 1; i < arrays.length; i++) {
+            set.retainAll(Arrays.asList(arrays[i]));
+        }
+        return (T[]) set.toArray();
+    }
+
+    public static <T> T[] union(final T[]... arrays) {
+        Set<T> set = new HashSet<>();
+        for (int i = 0; i < arrays.length; i++) {
+            set.addAll(Arrays.asList(arrays[i]));
+        }
+        return (T[]) set.toArray();
+    }
+
+    public static <T> T[] diff(final T[]... arrays) {
+        Set<T> set = new HashSet<>(Arrays.asList(arrays[0]));
+        for (int i = 1; i < arrays.length; i++) {
+            set.removeAll(Arrays.asList(arrays[i]));
+        }
+        return (T[]) set.toArray();
+    }
+
+    public <T> boolean includes(final T element) {
+        return Arrays.asList(this.array).contains(element);
+    }
+
+}
---
+++ src/main/java/test/arraysAsSetsTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import main.arraysAsSets;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static main.arraysAsSets.*;
+
+
+class arraysAsSetsTest {
+
+    private final String[] friends1 = {"vasya", "kolya", "petya"};
+    private final String[] friends2 = {"igor", "petya", "sergey", "vasya", "sasha"};
+    private final String[] friends3 = {"maxim", "rostislav", "petya"};
+    private final arraysAsSets includesTest1 = new arraysAsSets(new String[] {"rostislav", "maxim", "kolya"});
+    private final arraysAsSets includesTest2 = new arraysAsSets(new Integer[] {4, 13});
+    private final arraysAsSets includesTest3 = new arraysAsSets(new Integer[] {});
+
+    @Test
+    void intersectionTest() {
+        String[] expected = {"petya"};
+        Assertions.assertArrayEquals(expected, intersection(friends1, friends2, friends3));
+    }
+
+    @Test
+    void unionTest() {
+        String[] expected = {"igor", "rostislav", "maxim", "kolya", "sergey", "vasya", "sasha", "petya"};
+       Assertions.assertArrayEquals(expected, union(friends1, friends2, friends3));
+    }
+
+    @Test
+    void diffTest() {
+        String[] expected = {"kolya"};
+        Assertions.assertArrayEquals(expected, diff(friends1, friends2, friends3));
+    }
+
+    @Test
+    void includesTest() {
+        Assertions.assertTrue(includesTest1.includes("maxim"));
+        Assertions.assertFalse(includesTest2.includes(10));
+        Assertions.assertFalse(includesTest3.includes(5));
+    }
+
+}
